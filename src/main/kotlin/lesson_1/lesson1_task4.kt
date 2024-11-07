53a387f3af8e6000f54c575e38cf7af8ac6e5d56
fizik <fizik119@gmail.com>
2024-11-07 17:10:35 +0300

    KS-1-5 do lesson 4

--- src/main/kotlin/lesson_1/lesson1_task4.kt
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -7,6 +7,5 @@ fun main() {
     val seconds: Short = 6480
     val partOfYear: Double = 2.0547945205479453E-4
     val apogee: Int = 327000
-
     println("$length\n$age\n$partOfDay\n$seconds\n$partOfYear\n$apogee")
 }
