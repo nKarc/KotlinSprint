7c4b7d33240ef1a6111f69d8b8e3e2decdb4a0f0
fizik <fizik119@gmail.com>
2024-11-07 14:40:14 +0300

    KS-1-3 do lesson 3

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,5 +1,13 @@
 package org.example.lesson_1
 
 fun main() {
-    println("Тестовое решение к уроку 1 задаче 3")
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println("$year\n$hour\n$minute")
+
+    hour = 10
+    minute = 55
+    println("$hour:$minute")
 }
